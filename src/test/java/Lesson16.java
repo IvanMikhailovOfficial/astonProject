f31aac1e3397346c92a3940de419c9a735e2d9c4
IvanMikhailovOfficial <ivanm54@yandex.ru>
2024-09-26 11:35:43 +0100

    package rename lesson16

--- src/test/java/Lesson16.java
+++ src/test/java/Lesson16.java
@@ -8,7 +8,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class TestMts2 {
+public class Lesson16 {
     private static WebDriver webDriver;
 
     @BeforeClass
